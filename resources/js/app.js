aa0ec80917d031688f2f6f3cc4c5174428ea8f30
Alexander Guzman Rodriguez <alex_guzmanrodriguez_7@outlook.com>
2023-03-02 22:53:56 -0500

    Feat: Add vue component 'StudentComponent', set up vite.config.js and views to show component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import StudentComponent from './components/StudentComponent.vue';
+
+const app = createApp({});
+app.component('student-component', StudentComponent);
+app.mount('#app');
+
---
+++ resources/js/components/StudentComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="title">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "Student Management Component"
+        }
+    }
+
+}
+</script>
