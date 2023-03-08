fd619984a6aca7e9f48d91a290dd9c203edab16e
Alexander Guzman Rodriguez <alex_guzmanrodriguez_7@outlook.com>
2023-03-08 07:03:40 -0500

    Feat: Add bootstrap, create table to show students

--- resources/js/components/StudentComponent.vue
+++ resources/js/components/StudentComponent.vue
@@ -2,15 +2,41 @@
   <div class="title">
     <h2>{{ title }}</h2>
   </div>
+  <table class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <td>Document</td>
+        <td>Name</td>
+        <td>Last Name</td>
+        <td>Date of Birth</td>
+        <td>Email</td>
+        <td>Phone Number</td>
+        <td>Address</td>
+        <td>Is Active</td>
+        <td>Options</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>123456789</td>
+        <td>John</td>
+        <td>Doe</td>
+        <td>1990-05-02</td>
+        <td>johndoe@example.com</td>
+        <td>1234567</td>
+        <td>47 ST</td>
+        <td>true</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            title: "Student Management Component"
-        }
-    }
-
-}
+  data() {
+    return {
+      title: "Student Management Component",
+    };
+  },
+};
 </script>
